5f3314f83af9edefe4738a05f6da1aaddbd8ce9c
Anna Ellegaard Skytte <annaeskytte@gmail.com>
2025-11-04 15:25:01 +0100

    vite config edit

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import vue from '@vitejs/plugin-vue';
 import dns from 'dns';
-import {defineConfig} from 'vite';
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 // https://vitejs.dev/config/server-options.html#server-host
 dns.setDefaultResultOrder('verbatim');
@@ -10,5 +11,13 @@ export default defineConfig({
   plugins: [vue()],
   server: {
     port: 3000,
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        app: resolve(__dirname, 'app.html'),
+      },
+    },
   },
 });
