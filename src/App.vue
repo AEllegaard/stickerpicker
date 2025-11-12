744e64419e514f8e5cf82d52449eab3ab0b884bf
Anna Ellegaard Skytte <annaeskytte@gmail.com>
2025-11-12 12:49:13 +0100

    0,65

--- src/App.vue
+++ src/App.vue
@@ -510,7 +510,7 @@ function drawHandMaskIfNear(gfx, hand, faceBounds, maxDistPx = 20) {
     pinky:  [17,18,19,20],
   };
 
-  const baseW = handBaseWidth(kps) * 0.5; // reduced width for thinner fingers
+  const baseW = handBaseWidth(kps) * 0.65; // reduced width for thinner fingers
   const midW  = baseW * 0.8;
   const tipW  = baseW * 0.64;
 
